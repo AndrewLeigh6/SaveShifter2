4167fda1a8041eb0c679ae293b3393e4c72be2ba
Andrew <andrewleigh6@gmail.com>
2021-06-30 15:39:43 +0100

    Initial commit (but without .env this time)

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SaveShifter from "./components/SaveShifter/SaveShifter";
+
+function App() {
+  return (
+    <div className="App">
+      <SaveShifter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SaveShifter/SaveShifter.tsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from "react";
+import Snoowrap from "snoowrap";
+
+const SaveShifter = () => {
+  const [savedPosts, setSavedPosts] =
+    useState<Snoowrap.Listing<Snoowrap.Comment | Snoowrap.Submission> | null>(
+      null
+    );
+
+  const buildRequester = (username: string, password: string): Snoowrap => {
+    const snoowrap = new Snoowrap({
+      userAgent: "",
+      clientId: process.env.REACT_APP_CLIENT_ID,
+      clientSecret: process.env.REACT_APP_CLIENT_SECRET,
+      username: username,
+      password: password,
+    });
+
+    return snoowrap;
+  };
+
+  const getSavedPosts = async (
+    requester: Snoowrap
+  ): Promise<Snoowrap.Listing<Snoowrap.Comment | Snoowrap.Submission>> => {
+    const savedPosts = await requester.getMe().getSavedContent();
+    return savedPosts;
+  };
+
+  useEffect(() => {
+    if (!savedPosts) {
+      storeSavedPosts();
+    }
+
+    async function storeSavedPosts() {
+      const requester = authRequester();
+      if (requester) {
+        const savedPosts = await getSavedPosts(requester);
+        console.log("Saved posts", savedPosts);
+        setSavedPosts(savedPosts);
+      }
+    }
+
+    function authRequester(): Snoowrap | null {
+      const username = process.env.REACT_APP_FIRST_USERNAME;
+      const password = process.env.REACT_APP_FIRST_PASSWORD;
+
+      if (username && password) {
+        const snoowrap = buildRequester(username, password);
+        console.log("Logged in successfully");
+        return snoowrap;
+      } else {
+        console.log("Unable to login");
+        return null;
+      }
+    }
+  }, [savedPosts]);
+
+  const renderSavedPosts = (): (JSX.Element | null)[] | null => {
+    if (savedPosts) {
+      return savedPosts.map((savedPost) => {
+        if (isSubmission(savedPost) && savedPost.over_18 === false) {
+          return (
+            <div>
+              <p>{savedPost.title}</p>
+              <img src={savedPost.url} alt={savedPost.title} />
+            </div>
+          );
+        }
+        return null;
+      });
+    }
+
+    return null;
+  };
+
+  function isSubmission(
+    listing: Snoowrap.Comment | Snoowrap.Submission
+  ): listing is Snoowrap.Submission {
+    return (listing as Snoowrap.Submission).title !== undefined;
+  }
+
+  return (
+    <div>
+      <p>SaveShifter</p>
+      <div>{renderSavedPosts()}</div>
+    </div>
+  );
+};
+
+export default SaveShifter;
