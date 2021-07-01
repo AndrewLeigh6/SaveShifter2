0a1be4e8f0114a5f3ba3a313cf9fc1a7a679ab8c
Andrew <andrewleigh6@gmail.com>
2021-07-01 11:49:35 +0100

    Add render post text method

--- src/components/Posts/Post/Post.tsx
+++ src/components/Posts/Post/Post.tsx
@@ -6,29 +6,35 @@ interface PostProps {
 }
 
 const Post = (props: PostProps) => {
-  const renderImage = (savedPost: Snoowrap.Submission): JSX.Element | null => {
+  const renderImage = (): JSX.Element | null => {
     let src = "https://via.placeholder.com/108x108";
     let alt = "placeholder";
 
-    if (savedPost.preview) {
-      src = savedPost.preview.images[0].resolutions[0].url;
-      alt = savedPost.title;
+    if (props.savedPost.preview) {
+      src = props.savedPost.preview.images[0].resolutions[0].url;
+      alt = props.savedPost.title;
     }
 
-    let image = <img className="mr-5" src={src} alt={alt} />;
+    const image = <img className="mr-5" src={src} alt={alt} />;
 
     return image;
   };
 
-  return (
-    <div className="flex rounded-xl py-6 px-8 mb-4 bg-indigo-700">
-      {renderImage(props.savedPost)}
+  const renderPostText = (): JSX.Element => {
+    return (
       <div>
         <p>{props.savedPost.title}</p>
         <p className="text-red-500">
           {props.savedPost.subreddit_name_prefixed}
         </p>
       </div>
+    );
+  };
+
+  return (
+    <div className="flex rounded-xl py-6 px-8 mb-4 bg-indigo-700">
+      {renderImage()}
+      {renderPostText()}
     </div>
   );
 };
